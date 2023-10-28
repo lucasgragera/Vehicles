7ec1ce0c17baaa271727fd2b588dc75308c132a3
Lucas Gragera <lucasgragera1@gmail.com>
2023-10-28 19:58:36 -0300

    Updated Code

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,30 @@ public class Main {
     public static void main(String[] args) {
 
         // Instantiate objects of the Person class
-        Honda honda2 = new Honda("CRV", 2015);
+        Honda hondaCrv = new Honda("CRV", 2015);
         Ford fordFiesta = new Ford();
-        Audi audi1 = new Audi("A1", 2022);
-        Bmw bmw1 = new Bmw("M3", 2022);
-        Citroen citroen1 = new Citroen("C4", 2021);
-        Fiat fiat1 = new Fiat("Mobi", 2023);
-        MercedesBenz mercedesBenz1 = new MercedesBenz("AMG GTR", 2023);
-        Porsche porsche1 = new Porsche("911 GT3 RS", 2020);
-        Toyota toyota1 = new Toyota("Yaris", 2023);
+        Audi audiA1 = new Audi("A1", 2022);
+        Bmw bmwM3 = new Bmw("M3", 2022);
+        Citroen citroenC4 = new Citroen("C4", 2021);
+        Fiat fiatMobi = new Fiat("Mobi", 2023);
+        MercedesBenz mercedesBenzAmgGtr = new MercedesBenz("AMG GTR", 2023);
+        Porsche porsche911Gt3Rs = new Porsche("911 GT3 RS", 2020);
+        Toyota toyotaYaris = new Toyota("Yaris", 2023);
 
         // Use setters to initialize properties of fordFiesta
         fordFiesta.setModel("Focus");
         fordFiesta.setYear(2019);
 
         // Printing objects
-        System.out.println("Honda: Model - " + honda2.getModel() + ", Year - " + honda2.getYear());
+        System.out.println("Honda: Model - " + hondaCrv.getModel() + ", Year - " + hondaCrv.getYear());
         System.out.println("Ford: Model - " + fordFiesta.getModel() + ", Year - " + fordFiesta.getYear());
-        System.out.println("Audi: Model - " + audi1.getModel() + ", Year - " + audi1.getYear());
-        System.out.println("BMW: Model - " + bmw1.getModel() + ", Year - " + bmw1.getYear());
-        System.out.println("Citroen: Model - " + citroen1.getModel() + ", Year - " + citroen1.getYear());
-        System.out.println("Fiat: Model - " + fiat1.getModel() + ", Year - " + fiat1.getYear());
-        System.out.println("MercedesBenz: Model - " + mercedesBenz1.getModel() + ", Year - " + mercedesBenz1.getYear());
-        System.out.println("Porsche: Model - " + porsche1.getModel() + ", Year - " + porsche1.getYear());
-        System.out.println("Toyota: Model - " + toyota1.getModel() + ", Year - " + toyota1.getYear());
+        System.out.println("Audi: Model - " + audiA1.getModel() + ", Year - " + audiA1.getYear());
+        System.out.println("BMW: Model - " + bmwM3.getModel() + ", Year - " + bmwM3.getYear());
+        System.out.println("Citroen: Model - " + citroenC4.getModel() + ", Year - " + citroenC4.getYear());
+        System.out.println("Fiat: Model - " + fiatMobi.getModel() + ", Year - " + fiatMobi.getYear());
+        System.out.println("MercedesBenz: Model - " + mercedesBenzAmgGtr.getModel() + ", Year - " + mercedesBenzAmgGtr.getYear());
+        System.out.println("Porsche: Model - " + porsche911Gt3Rs.getModel() + ", Year - " + porsche911Gt3Rs.getYear());
+        System.out.println("Toyota: Model - " + toyotaYaris.getModel() + ", Year - " + toyotaYaris.getYear());
 
         System.out.println();
 
